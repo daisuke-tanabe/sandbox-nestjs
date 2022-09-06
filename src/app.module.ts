e4aa8c708d8a64e6311aee918cccc135a1901fbc
daisuke <daisuke@jbpress.co.jp>
2022-09-06 23:12:27 +0900

    chore: コマンドでリソースを生成する

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { CatsController } from './cat.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CatsController],
+  imports: [UsersModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/cat.controller.ts
+++ src/cat.controller.ts
@@ -12,20 +12,20 @@ export class CatsController {
     return 'This action adds a new cat';
   }
 
-  @Get()
-  findAll(@Query() query: ListAllEntities) {
-    return `This action returns all cats (limit: ${query.limit} items)`;
-  }
+  // @Get()
+  // findAll(@Query() query: ListAllEntities) {
+  //   return `This action returns all cats (limit: ${query.limit} items)`;
+  // }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
     return `This action returns a #${id} cat`;
   }
 
-  @Put(':id')
-  update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
-    return `This action updates a #${id} cat`;
-  }
+  // @Put(':id')
+  // update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
+  //   return `This action updates a #${id} cat`;
+  // }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
